f10ca93ccb2a668746317ac45e480afb423df958
huythong95 <t.huythonght95@gmail.com>
2019-09-06 08:58:39 +0700

    create Sign up form

---
+++ src/main/java/com/codegym/rentapartmentbe/Form/SignUpForm.java
@@ -0,0 +1,84 @@
+package com.codegym.rentapartmentbe.Form;
+
+import org.hibernate.annotations.NaturalId;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.Set;
+
+public class SignUpForm {
+    @NotBlank
+    @Size(min = 3, max = 50)
+    private String fullName;
+
+    @NotBlank
+    @NaturalId
+    @Size(min = 3, max = 50)
+    private String name;
+
+    @NaturalId
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min = 6, max = 100)
+    private String passWord;
+
+    @NotBlank
+    @NaturalId
+    @Size(min = 10, max = 11)
+    private String phoneNumber;
+
+    private Set<String> role;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Set<String> getRole() {
+        return role;
+    }
+
+    public void setRole(Set<String> role) {
+        this.role = role;
+    }
+}
