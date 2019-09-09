c17bb63c9bfc2398b6ecb5e3936ec0139f1304e0
huythong95 <t.huythonght95@gmail.com>
2019-09-09 08:46:35 +0700

    sua repo landlord

--- src/main/java/com/codegym/rentapartmentbe/repository/LandLordRepository.java
+++ src/main/java/com/codegym/rentapartmentbe/repository/LandLordRepository.java
@@ -4,9 +4,11 @@ import com.codegym.rentapartmentbe.entity.LandLord;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface LandLordRepository extends CrudRepository<LandLord, Long> {
-    LandLord findByName(String name);
+    Optional<LandLord> findByName(String name);
     Boolean existsByName(String name);
     Boolean existsByEmail(String email);
 }
